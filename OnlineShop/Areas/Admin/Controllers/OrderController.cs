82e84ae460335e6663706fd2ddfa760d0387a1d8
004D7M744 <004D7M744@IBM-PF3PCJ1T>
2023-01-18 15:36:34 +0530

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+using System;
+using System.Linq;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Orders.ToList());
+        }
+    }
+}
---
+++ OnlineShop/Data/Migrations/20230113055556_addedusertable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Data.Migrations
+{
+    public partial class addedusertable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LockOutEnd",
+                table: "AspNetUsers",
+                newName: "LockoutEnd");
+
+            migrationBuilder.RenameColumn(
+                name: "LockOutEnabled",
+                table: "AspNetUsers",
+                newName: "LockoutEnabled");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                newName: "LockOutEnd");
+
+            migrationBuilder.RenameColumn(
+                name: "LockoutEnabled",
+                table: "AspNetUsers",
+                newName: "LockOutEnabled");
+        }
+    }
+}
